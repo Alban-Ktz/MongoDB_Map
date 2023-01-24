62b734b030a1009ededc64344abf73cd88c89ada
Yanis Rezgui <chayano57@gmail.com>
2023-01-24 22:41:04 +0100

    commentaire sur index.php

--- src/index.php
+++ src/index.php
@@ -1,16 +1,27 @@
 <?php
 // Code source permettant d'accéder aux données parking du Grand Nancy
 
+//Autoload de composer, pour load toutes les dépendances de composer
 require_once('../vendor/autoload.php');
 
+//Tableau qui va contenir les données à inserer
 $pis = [];
 
+//On crée une instance de MongoDB (Une connexion en gros) et on selectionne la BDD 'tdmongo'
 $db = (new MongoDB\Client('mongodb://mongodb'))->selectDatabase('tdmongo');
-$data = json_decode(file_get_contents('https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Parking/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=nom%2Cadresse%2Cplaces%2Ccapacite&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson'));
+
+//Ici on récupère les données de l'API des parkings, un peu comme un fetch en JS, puis un les transformes en format json avec la fonction json_decode()
+$data = json_decode(file_get_contents('...'));
+
+//Dans la BDD on crée une collection nommée 'pis' (Collection = table en MySQL)
 $db->createCollection('pis');
+
+//On selectionne la collection pis créée juste au dessus
 $db = $db->selectCollection('pis');
 
+//Pour chaque parking 
 foreach ($data->features as $feature) {
+       //On crée un Objet qui est au format GeoJSON cf. https://fr.wikipedia.org/wiki/GeoJSON
   $pi = [
     'name' => $feature->attributes->NOM,
     'address' => $feature->attributes->ADRESSE,
@@ -24,9 +35,12 @@ foreach ($data->features as $feature) {
     'places' => $feature->attributes->PLACES,
     'capacity' => $feature->attributes->CAPACITE,
   ];
+
+  //On ajoute l'objet dans le tableau déclaré au tout début
   $pis[] = $pi;
 }
 
+//Si  le tableau n'est pas vide on insert TOUTES les données dans la BDD avec insertMany. Pour inserer seulement un objet on utilise la méthode insertOne
 if (count($pis) > 0) {
   $res = $db->insertMany($pis);
 }
