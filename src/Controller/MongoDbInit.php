530fffb2c2158627e33c30155a9b3e1d7a39de9e
Yanis Rezgui <chayano57@gmail.com>
2023-01-25 21:38:32 +0100

    api

---
+++ src/Controller/MongoDbInit.php
@@ -0,0 +1,26 @@
+<?php
+    $db = (new MongoDB\Client('mongodb://mongodb'))->selectDatabase('tdmongo');
+    $table= $db->selectCollection('pis');
+    $items= $table->find([]);
+    $i=0;
+    $api=array();
+    foreach ($items as $item)
+    {
+
+    $array=Array (
+            "name" => $item->name,
+            "ad"=>$item->address,
+            "desc"=>$item->description,
+            "x"=>$item->geometry->x,
+            "y"=>$item->geometry->y,
+            "places"=>$item->places,
+            "capacities"=>$item->capacity,
+        );
+            $i++;
+            array_push($api,$array);
+
+    }
+    $jsonapi=json_encode($api,JSON_UNESCAPED_UNICODE);
+    file_put_contents("myfile.json",$jsonapi);
+    echo($jsonapi);
+    ?>
